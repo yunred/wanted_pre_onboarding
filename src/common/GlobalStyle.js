4a0a525f763184c9a03cca7d9eb2cde68bdc73a2
SEOYUN KIM <nado0903@gmail.com>
2022-01-08 20:39:55 +0900

    nav sm,md사이즈 UI 완성

---
+++ src/common/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*{
+  padding:0;
+  margin:0;
+  box-sizing:border-box;
+}
+body{
+  font-size:14px;
+  font-family: sans-serif;
+}
+
+`;
+
+export default GlobalStyle;
